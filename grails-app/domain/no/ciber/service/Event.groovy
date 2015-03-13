8aaba7383c629033440c028f0040adf6a45dba84
oyvindBjerke <o.t.bjerke@gmail.com>
2015-03-13 15:34:42 +0100

    Added optional attribute to Event; users.

--- grails-app/domain/no/ciber/service/Event.groovy
+++ grails-app/domain/no/ciber/service/Event.groovy
@@ -1,19 +1,18 @@
 package no.ciber.service
 
-import grails.rest.Resource
-
-@Resource(uri = '/events', formats = ['json', 'xml'])
 class Event {
 
     String name
     Date createdDate = new Date()
     String description
     Date eventDate
+    static hasMany = [ users: String]
 
     static constraints = {
         name blank: false
         description blank: false
         eventDate blank: false
         createdDate blank: false
+        users nullable: true
     }
 }
