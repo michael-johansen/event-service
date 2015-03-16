767c56ffcbb19b5943429db9561157ecf24ec8c6
oyvindBjerke <o.t.bjerke@gmail.com>
2015-03-16 11:53:18 +0100

    Changed application to run on root

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -7,8 +7,7 @@ class UrlMappings {
             }
         }
 
-        "/"(view:"/index")
         "500"(view:'/error')
-        "/events"(resources: "event")
+        "/"(resources: "event")
 	}
 }
