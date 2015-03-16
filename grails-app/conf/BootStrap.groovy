0599f3e047651cefc259186148b64ac08cbc2d01
oyvindBjerke <o.t.bjerke@gmail.com>
2015-03-16 12:25:55 +0100

    Test data is now only created for development mode

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,10 +1,13 @@
+import grails.util.Environment
 import no.ciber.service.Event
 
 class BootStrap {
 
     def init = { servletContext ->
-        new Event(name: "My first event", description: "This is an example event", eventDate: new Date(), createdDate: new Date()).save()
-        new Event(name: "My second event", description: "This is annother example event", eventDate: new Date(), createdDate: new Date()).save()
+        if(Environment.current == Environment.DEVELOPMENT) {
+            new Event(name: "My first event", description: "This is an example event", eventDate: new Date(), createdDate: new Date()).save()
+            new Event(name: "My second event", description: "This is annother example event", eventDate: new Date(), createdDate: new Date()).save()
+        }
     }
     def destroy = {
     }
