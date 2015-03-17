e995d81a1f501d786e99c741ba1bdd24e7ff4c18
oyvindBjerke <o.t.bjerke@gmail.com>
2015-03-17 14:49:06 +0100

    Added logging for when an event is created without a user (had to override the save() method)

--- grails-app/controllers/no/ciber/service/EventController.groovy
+++ grails-app/controllers/no/ciber/service/EventController.groovy
@@ -3,10 +3,15 @@ package no.ciber.service
 import grails.rest.RestfulController
 
 import grails.transaction.Transactional
+import org.apache.commons.logging.LogFactory
+import org.codehaus.groovy.grails.web.servlet.HttpHeaders
 import org.joda.time.DateTime
+
+import static org.springframework.http.HttpStatus.CREATED
 
 @Transactional(readOnly = true)
 class EventController extends RestfulController {
+    private static final LOG = LogFactory.getLog(EventController.class)
     static responseFormats = ['json', 'xml']
 
     EventController() {
@@ -38,4 +43,46 @@ class EventController extends RestfulController {
         result
     }
 
+    @Override
+    @Transactional
+    def save() {
+        if(handleReadOnly()) {
+            return
+        }
+        def event = createResource()
+
+        event.validate()
+        if (event.hasErrors()) {
+            respond event.errors, view:'create' // STATUS CODE 422
+            return
+        }
+
+        event.save flush:true
+
+        if(event.users.empty) {
+            LOG.info(String.format("Event with id: %S was saved without any users", event.id))
+        }
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: "${resourceName}.label".toString(), default: resourceClassName), event.id])
+                redirect event
+            }
+            '*' {
+                response.addHeader(HttpHeaders.LOCATION,
+                        g.createLink(
+                                resource: this.controllerName, action: 'show',id: event.id, absolute: true,
+                                namespace: hasProperty('namespace') ? this.namespace : null ))
+                respond event, [status: CREATED]
+            }
+        }
+
+    }
+
+    @Override
+    protected Event createResource() {
+        Event instance = Event.newInstance()
+        bindData instance, getObjectToBind()
+        instance
+    }
 }
